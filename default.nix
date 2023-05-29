d8e82f83c5261d3d1791f036c75bf6f85d4d9100
Blake Smith <blakesmith0@gmail.com>
2023-05-29 06:39:31 -0500

    Add initial naersk build

--- default.nix
+++ default.nix
@@ -1,26 +1,7 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/release-21.11.tar.gz") {},
-  lib ? pkgs.lib,
-  rustPlatform ? pkgs.rustPlatform,
-}:
-with rustPlatform;
-
-buildRustPackage rec {
-  pname = "binpocket";
-  version = "0.1.0";
-
-  src = builtins.filterSource
-    (path: type: type != "directory" || baseNameOf path != "target")
-    ./.;
-  cargoSha256 = "1yqkxq0076qq9d4am6mznsprv1qz71jlm0pdpcsqn5r9hyc2d421";
-
-  # doc tests fail due to missing dependency
-  doCheck = false;
-
-  PROTOC = "${pkgs.protobuf}/bin/protoc";
-
-  meta = with lib; {
-    description = "Binpocket server";
-    homepage = "https://blakesmith.me";
-    license = with licenses; [ mit asl20 ];
-  };
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).defaultNix
